120f8fc7fda83f12a17218a7b36d81842dba2901
ThomasF85 <thomas.foeldi@gmail.com>
2022-04-13 14:15:30 +0200

    add CRUD operations

--- routes/jokes.js
+++ routes/jokes.js
@@ -27,7 +27,7 @@ let jokes = [
  * Create a GET /joke route, that returns all jokes.
  */
 router.get("/", (req, res, next) => {
-  // …
+  res.status(200).json(jokes);
 });
 
 /**
@@ -35,27 +35,65 @@ router.get("/", (req, res, next) => {
  * Create a GET /joke/:id route, that returns the joke for the given id.
  */
 
-// …
+router.get("/:id", (req, res, next) => {
+  const { id } = req.params;
+  const joke = jokes.find((joke) => joke.id === id);
+  if (joke) {
+    res.status(200).json(joke);
+  } else {
+    next();
+  }
+});
 
 /**
  * Exercise 3
  * Create a POST /joke route, that adds a new joke to the array.
  */
 
-// …
+router.post("/", (req, res, next) => {
+  const newJoke = { id: nanoid(), ...req.body };
+  jokes.push(newJoke);
+  res.status(201).json(newJoke);
+});
 
 /**
  * Exercise 4
  * Create a PATCH /joke/:id route, that updates the joke for the given id.
  */
 
-// …
+router.patch("/:id", (req, res, next) => {
+  const { id } = req.params;
+  const jokeIndex = jokes.findIndex((joke) => joke.id === id);
+  if (jokeIndex !== -1) {
+    jokes[jokeIndex] = {
+      ...jokes[jokeIndex],
+      ...req.body,
+    };
+
+    res.status(200).json(jokes[jokeIndex]);
+  } else {
+    next();
+  }
+});
 
 /**
  * Exercise 5
  * Create a DELETE /joke/:id route, that deletes the joke for the given id.
  */
 
-// …
+router.delete("/:id", (req, res, next) => {
+  const { id } = req.params;
+  const jokeIndex = jokes.findIndex((joke) => joke.id === id);
+
+  if (jokeIndex === -1) {
+    next();
+  } else {
+    jokes = jokes.filter((joke) => joke.id !== id);
+
+    res.status(200).json({
+      deletedId: id,
+    });
+  }
+});
 
 export default router;
